47b1d0e1384a03926218fee5157c0ed23c2f61f7
serge <sergei-msg@tut.by>
2024-10-01 21:26:32 +0300

    add files for review

---
+++ src/main/java/by/photoshop/smelnik/ui/pages/search/SearchQueriesXPath.java
@@ -0,0 +1,7 @@
+package by.photoshop.smelnik.ui.pages.search;
+
+public class SearchQueriesXPath {
+    public static final String SEARCH_FIELD_BASE_PAGE = "//input[@id='findtext']";
+    public static final String SEARCH_BUTTON_START_BASE_PAGE = "//button/i";
+    public static final String SEARCH_RESULT1 = "//span[@itemprop='name' and text() = 'Зеркальный фотоаппарат Canon EOS 5D Mark IV BODY.']";
+}
